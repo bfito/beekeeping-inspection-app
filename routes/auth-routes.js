2565e488639edfcdd4d3148f38523b174fc1d18c
JPZ <jeanpaulzune@gmail.com>
2017-03-06 14:05:45 -0500

    Fixed some bugs within passport login page...

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,184 +1,114 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-
-const User = require('../models/user-model.js');
-
+const express    = require("express");
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy; // Specific passport startegy. Local.
 const authRoutes = express.Router();
+// Bcrypt to encrypt passwords
+const bcrypt     = require("bcrypt");
+
+// User model
+const User       = require("../models/user-model");
 
 
-authRoutes.get('/signup', (req, res, next) => {
-  res.render('auth/signup-view.ejs');
+authRoutes.get("/signup", (req, res, next) => {
+  res.render("auth/signup-view");
 });
 
-authRoutes.post('/signup', (req, res, next) => {
+authRoutes.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  if (username === '' || password === '') {
-    res.render('auth/signup-view.ejs', {
-      errorMessage: 'Please fill out both username and password foo\'!'
-    });
+  if (username === "" || password === "") {
+    res.render("auth/signup-view", { message: "Indicate username and password" });
     return;
   }
 
-  User.findOne(
-    { username: username },
-    { username: 1 },
+  User.findOne({ username: username }, "username", (err, user) => { //"username" means: just display the username field.
+    if (user !== null) {
+      res.render("auth/signup-view", { message: "The username already exists" });
+      return;
+    }
 
-    (err, foundUser) => {
+    const salt     = bcrypt.genSaltSync(10);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
       if (err) {
-        next(err);
-        return;
+        res.render("auth/signup-view", { message: "Something went wrong" });
+      } else {
+        // 'success' is made up. Can be called whatever you want. You are going to use it in the view to retrieve the message
+        req.flash('success', 'You have been successfully registered. Try Logging in!');
+        res.redirect("/");
       }
-
-      if (foundUser !== null) {
-        res.render('auth/signup-view.ejs', {
-          errorMessage: 'The username already exists'
-        });
-        return;
-      }
-
-      const salt = bcrypt.genSaltSync(10);
-      const hashPass = bcrypt.hashSync(password, salt);
-
-      const userInfo = {
-        username: username,
-        password: hashPass
-      };
-
-      const theUser = new User(userInfo);
-
-      theUser.save((err) => {
-        if (err) {
-          res.render('auth/signup-view.ejs', {
-            errorMessage: 'Oops! There was a problem. Try again later.'
-          });
-          return;
-        }
-
-        res.redirect('/');
-      });
     });
+  });
 });
 
 
-authRoutes.get('/login', (req, res, next) => {
-  res.render('auth/login-view.ejs');
-});
-
-authRoutes.post('/login', (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  if (username === '' || password === '') {
-    res.render('auth/login-view.ejs', {
-      errorMessage: 'Indicate a username and password to log in.'
-    });
-    return;
-  }
-
+// Handling different cases of the findOne method.
+passport.use(new LocalStrategy((username, password, next) => {
   User.findOne({ username: username }, (err, user) => {
     if (err) {
-      next(err);
-      return;
+      return next(err);
     }
-
     if (!user) {
-      res.render('auth/login-view.ejs', {
-        errorMessage: 'The username doesn\'t exist'
-      });
-      return;
-    }// routes/auth.js
-const express = require('express');
-const bcrypt = require('bcrypt');
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+    // If you want to do some extra logic, do it here and not in authRoutes
+    return next(null, user);
+  });
+}));
 
-const User = require('../models/user-model');
-
-const router = express.Router();
-const bcryptSalt = 10;
-
-
-router.get('/signup', (req, res, next) => {
-  res.render('auth/signup-view', {
-    errorMessage: ''
+//-------------------- LOG IN HANDLER ---------------------------
+authRoutes.get("/login", (req, res, next) => {
+  res.render("auth/login-view", {
+    // If there is an error, then the error will be specified. The if statement is inside the login view.
+    errorMessage: req.flash('error') //The catcher, waiting for the ball to be thrown. If there is no error, there wont be any error to display (duh!)
+    // So how to use it? If we redirect to this address, we want to send or throw "req.flash('error', 'No success in log in')"; so that the catcher can actually grab the information.
+    // Remember, do this req.flash before redirecting so that the errorMessage is passed on.
   });
 });
 
-router.post('/signup', (req, res, next) => {
-  const nameInput = req.body.name;
-  const emailInput = req.body.email;
-  const passwordInput = req.body.password;
+authRoutes.post("/login", passport.authenticate("local", {
+  successReturnToOrRedirect: "/", //If i try to access kgb-files(which are protected), then its going to direct me to login. If I log In successfully, it will directly redirect me to kgb-files!!
+  failureRedirect: "/login",
+  failureFlash: true,
+  successFlash: 'Successful Log In!',
+  passReqToCallback: true
+}));
 
-  if (emailInput === '' || passwordInput === '') {
-    res.render('auth/signup', {
-      errorMessage: 'Enter both email and password to sign up.'
-    });
-    return;
-  }
+// authRoutes.get("/auth/facebook", passport.authenticate("facebook"));
+//
+// authRoutes.get("/auth/facebook/callback", passport.authenticate("facebook", {
+//   successRedirect: "/",
+//   failureRedirect: "/login"
+// }));
 
-  User.findOne({ email: emailInput }, '_id', (err, existingUser) => {
-    if (err) {
-      next(err);
-      return;
-    }
+// authRoutes.get("/auth/google", passport.authenticate("google", {
+//   scope: ["https://www.googleapis.com/auth/plus.login",
+//           "https://www.googleapis.com/auth/plus.profile.emails.read"]
+//           //Scope defines what we are able to access we request information from google. Basically the points that tell me what I will be able to see from the user (i.e blue box of fb telling user what im requesting)
+// }));
 
-    if (existingUser !== null) {
-      res.render('auth/signup', {
-        errorMessage: `The email ${emailInput} is already in use.`
-      });
-      return;
-    }
+// authRoutes.get("/auth/google/callback", passport.authenticate("google", {
+//   successRedirect: "/",
+//   failureRedirect: "/login"
+// }));
+//--------------------------------------------------------------
 
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashedPass = bcrypt.hashSync(passwordInput, salt);
-
-    const userSubmission = {
-      name: nameInput,
-      email: emailInput,
-      password: hashedPass
-    };
-
-    const theUser = new User(userSubmission);
-
-    theUser.save((err) => {
-      if (err) {
-        res.render('auth/signup', {
-          errorMessage: 'Something went wrong. Try again later.'
-        });
-        return;
-      }
-
-      res.redirect('/');
-    });
-  });
-});
-
-
-module.exports = router;
-
-
-    // This is the more important part of the code to verify password..
-    if (bcrypt.compareSync(password, user.password)) {
-      // Current will have sometingin there if user logged in succesfully.
-      req.session.currentUser = user;
-      res.redirect('/');
-    } else {
-      res.render('auth/login-view.ejs', {
-        errorMessage: 'The password is incorrect'
-      });
-      return;
-    }
-  });
-});
-
-authRoutes.get('/logout', (req, res, next) => {
-  req.session.destroy((err) => {
-    if (err) {
-      next(err);
-      return;
-    }
-    res.redirect('/');
-  });
+authRoutes.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success', 'You have successfully logged out.'); //Actually sending the ('success')ball to a catcher waiting for a success ball. In this case the .get('/') is awaiting a success ball.
+  res.redirect('/');
 });
 
 module.exports = authRoutes;
