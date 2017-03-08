46a41a9edb3030250907bea3022de5d61ae2cabb
JPZ <jeanpaulzune@gmail.com>
2017-03-07 21:26:06 -0500

    Committing to commit.

--- routes/hives-routes.js
+++ routes/hives-routes.js
@@ -1,120 +1,55 @@
 const express = require('express');
+const ensure = require('connect-ensure-login');
+const multer = require('multer');
+const Hive = require('../models/hive-model');
 
-const Hive = require('../models/hive-model.js');
+const router  = express.Router();
+const hivesRoutes = express.Router();
+// const uploads = multer({ dest: '__dirname' + '/../public/uploads/' });
 
-const router = express.Router();
-
-
-router.get('/hives', (req, res, next) => {
-  Hive.find((err, hives) => {
-    if (err) {
-      next(err);
-      return;
-    }
-
-      // display views/hives/index.ejs
-    res.render('hives/hive-index', {
-      hives: hives
-    });
-  });
-});
-
-router.get('/hives/new', (req, res, next) => {
-    // display views/hives/new.ejs
-  res.render(('hives/new-view'), {
-    errorMessage: ''
-  });
-});
-
-router.post('/hives', (req, res, next) => {
-  const hiveInfo = {
-    name: req.body.name,
-    price: req.body.dateCreated,
-    description: req.body.comment,
-    // imageUrl: req.body.imageUrl,
-  };
-
-  const theHive = new Hive(hiveInfo);
-
-  theHive.save((err) => {
-    if (err) {
-      res.render('hives/new', {
-        errorMessage: 'Oh no! Validation Failzsed!',
-        errors: theHive.errors
-      });
-      return;
-    }
-
-      // redirect to http://localhost:3000/hives
-      //                                  ---------
-      //                                       |
-      //              --------------------------
-      //              |
-    res.redirect('/hives');
-  });
-});
-
-// res.render('hives/show', {
-//   hive: hiveDoc
-// });
-
-router.get('/hives/:id/edit', (req, res, next) => {
-  const hiveId = req.params.id;
-
-  Hive.findById(hiveId, (err, hiveDoc) => {
+hivesRoutes.get('/hives/index', ensure.ensureLoggedIn(), (req, res, next) => {
+  Hive.find({owner: req.user._id}, (err, myHives) => {
     if (err) { return next(err); }
-    res.render('hives/edit', {
-      hive: hiveDoc
-    });
+
+    res.render('hives/hive-index', { hives: myHives });
   });
 });
 
+hivesRoutes.get('/hives/new', ensure.ensureLoggedIn(), (req, res, next) => {
+  res.render('hives/new-view.ejs', {
+    message: req.flash('success')
+  });
+});
 
-router.post('/hives/:id', (req, res, next) => {
-  const hiveId = req.params.id;
-  const hiveUpdates = {
-    name: req.body.name,
-    price: req.body.dateCreated,
-    description: req.body.comment,
-    // imageUrl: req.body.imageUrl,
-};
-  // db.hives.updateOne({ _id: hive }, { $set: hiveUpdates })
-  Hive.findByIdAndUpdate(hiveId, hiveUpdates, (err, hive) => {
-    if (err){
-      next(err);
-      return;
+// router.post('/hives', ensureAuthenticated, (req, res, next) => {
+hivesRoutes.post('/hives',
+  ensure.ensureLoggedIn(),
+  // ('picture') refers to name="picture" in the form
+  // uploads.single('picture'),
+
+  //  (req, res, next) => {
+  //    const filename = req.file.filename;
+
+    new Hive ({
+      name:  req.body.name,
+      dateCreated: req.body.dateCreated,
+      comment:  req.body.comment,
+      // picture: `/uploads/${filename}`,
+      owner: req.user._id   // <-- we add the user ID
     }
-      res.redirect('/hives');
-    });
-  });
+  ));
 
-  router.post('/hives/:id/delete', (req, res, next) => {
-    const hiveId = req.params.id;
-
-    console.log(hiveId);
-
-    // db.hives.deleteOne({_id: hiveId })
-    Hive.findByIdAndRemove(hiveId, (err, hive) => {
-      if (err) {
-        next(err);
-        return;
-      }
-      res.redirect('/hives');
-    });
-  });
-
-router.get('/hives/:id', (req, res, next) => {
-  let hiveId = req.params.id;
-
-  Hive.findById(hiveId, (err, hiveDoc) => {
+  newHive.save ((err) => {
     if (err) {
       next(err);
       return;
+    } else {
+      req.flash('success', 'Your hive has been created');
+      res.redirect('/hives/new');
     }
-    res.render('hives/show', {
-      hive: hiveDoc
-    });
   });
-});
 
-module.exports = router;
+
+
+
+module.exports = hivesRoutes;
